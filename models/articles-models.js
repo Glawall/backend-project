5b8ba5081fcab5cc5e022d5ef03040358c02d7c2
Glawall <glawall217@gmail.com>
2024-04-15 17:19:09 +0100

    done

--- models/articles-models.js
+++ models/articles-models.js
@@ -12,10 +12,6 @@ function fetchArticle(article_id) {
 }
 
 function fetchArticles() {
-
-    // return db.query(`SELECT article_id, comment_count, title, author, created_at, votes, topic, article_img_url FROM articles ORDER BY created_at DESC;`)})
-    // .then(({rows}) => {
-    //     let fullArr = rows
         return db
         .query(
             "SELECT articles.article_id, articles.title, articles.author, articles.created_at, articles.votes, articles.topic, articles.article_img_url, COUNT(articles.article_id) comment_count FROM articles JOIN comments on comments.article_id = articles.article_id GROUP BY articles.article_id ORDER BY created_at DESC"
