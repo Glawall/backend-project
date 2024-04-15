170e7a24b0af17eceaa643ddbddbf211a20552d9
Glawall <glawall217@gmail.com>
2024-04-15 11:14:36 +0100

    api/topics responds with an array of topic objects with correct properties

---
+++ __tests__/endpoints.test.js
@@ -0,0 +1,37 @@
+const app = require("../app")
+const request = require("supertest")
+const db = require("../db/connection")
+const seed = require("../db/seeds/seed")
+const data = require("../db/data/test-data/index.js")
+
+beforeEach(()=> {
+    return seed(data)
+})
+
+afterAll(() => {
+    return db.end()
+})
+
+describe("/api/healthcheck", () => {
+    test("GET 200: responds with a 200 status code", () => {
+        return request(app).get("/api/healthcheck").expect(200)
+    })
+})
+
+describe("/api/topics", () => {
+    test("GET 200: responds with an array of topic objects, with each object having the properites of slug & description", () => {
+        return request(app)
+        .get("/api/topics")
+        .expect(200)
+        .then(({body}) => {
+           let {topics} = body
+           console.log(topics)
+           expect(topics.length).toBe(3)
+           topics.forEach((topic) => {
+            expect(typeof topic.description).toBe("string")
+            expect(typeof topic.slug).toBe("string")
+           })
+        })
+    })
+
+})
---
+++ controllers/topics-controller.js
@@ -0,0 +1,14 @@
+const {fetchTopics} = require("../models/topics-model.js")
+
+const getTopics = (req, res, next) => {
+    fetchTopics().then((topics) => {
+        res.status(200).send({topics});
+})
+.catch((err) => {
+    next(err)
+})
+}
+
+
+
+module.exports = {getTopics}
