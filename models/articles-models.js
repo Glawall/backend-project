c3925180eeb495f62f97cadb702e83b2cc334915
Glawall <glawall217@gmail.com>
2024-04-17 11:45:34 +0100

    Completes task 11, topic query included on /api/articles, returns an array of articles with that topic, or error message if no topic exists

--- models/articles-models.js
+++ models/articles-models.js
@@ -11,14 +11,24 @@ function fetchArticle(article_id) {
     });
 }
 
-function fetchArticles() {
-  return db
-    .query(
-      "SELECT articles.article_id, articles.title, articles.author, articles.created_at, articles.votes, articles.topic, articles.article_img_url, COUNT(articles.article_id) comment_count FROM articles LEFT JOIN comments on comments.article_id = articles.article_id GROUP BY articles.article_id ORDER BY created_at DESC"
-    )
-    .then(({ rows }) => {
-      return rows;
-    });
+function fetchArticles(topic) {
+  let stringQuery = `SELECT articles.article_id, articles.title, articles.author, articles.created_at, articles.votes, articles.topic, articles.article_img_url, COUNT(articles.article_id) comment_count FROM articles LEFT JOIN comments on comments.article_id = articles.article_id `;
+
+  const queryVals = [];
+
+  if (topic) {
+    stringQuery += `WHERE topic =$1 GROUP BY articles.article_id ORDER BY created_at DESC`;
+    queryVals.push(topic);
+  } else {
+    stringQuery += `GROUP BY articles.article_id ORDER BY created_at DESC`;
+  }
+
+  return db.query(stringQuery, queryVals).then(({ rows }) => {
+    if (rows.length === 0) {
+      return Promise.reject({ status: 404, message: "topic not found" });
+    }
+    return rows;
+  });
 }
 
 function checkArticleExists(article_id) {
@@ -31,18 +41,23 @@ function checkArticleExists(article_id) {
     });
 }
 
-function updateArticleVotes(article_id, inc_votes){
-if(!inc_votes.inc_votes){
-  return Promise.reject({ status: 400, message: "Bad request" });
-}
-return db.query(`UPDATE articles SET votes = ${inc_votes.inc_votes} WHERE article_id=$1 RETURNING *`, [article_id]).then(({rows}) => {
-  return rows[0]
-})
+function updateArticleVotes(article_id, inc_votes) {
+  if (!inc_votes.inc_votes) {
+    return Promise.reject({ status: 400, message: "Bad request" });
+  }
+  return db
+    .query(
+      `UPDATE articles SET votes = ${inc_votes.inc_votes} WHERE article_id=$1 RETURNING *`,
+      [article_id]
+    )
+    .then(({ rows }) => {
+      return rows[0];
+    });
 }
 
 module.exports = {
   fetchArticle,
   fetchArticles,
   checkArticleExists,
-  updateArticleVotes
+  updateArticleVotes,
 };
