b26ffd670fcf2f1016ce3056ea5373b0d10aec41
Glawall <glawall217@gmail.com>
2024-04-17 12:16:20 +0100

    completes task 12, adds comment-count to /api/articles/:article_id

--- __tests__/endpoints.test.js
+++ __tests__/endpoints.test.js
@@ -47,7 +47,7 @@ describe("/api", () => {
   });
 });
 
-describe("api/articles/aritcle_id", () => {
+describe("api/articles/article_id", () => {
   test("GET 200, responds with a sngular article object", () => {
     return request(app)
       .get("/api/articles/1")
@@ -64,6 +64,27 @@ describe("api/articles/aritcle_id", () => {
         expect(typeof article.article_img_url).toBe("string");
       });
   });
+  test("GET 200, responds with a singular article object with comment count-included", () => {
+    return request(app)
+    .get("/api/articles/1")
+    .expect(200)
+    .then(({body}) => {
+      const {article} = body
+      expect(article).toEqual(
+      expect.objectContaining({
+        article_id: 1,
+        article_img_url: "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: "2020-07-09T20:11:00.000Z",
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        votes: 100,
+        comment_count: expect.any(Number)
+    })
+    )
+  })
+})
   test("GET 404: sends an error message when given a valid but non-exists id", () => {
     return request(app)
       .get("/api/articles/999")
@@ -146,7 +167,7 @@ describe("/api/articles", () => {
           expect(typeof article.article_id).toBe("number");
           expect(typeof article.title).toBe("string");
           expect(typeof article.author).toBe("string");
-          expect(typeof article.comment_count).toBe("string");
+          expect(typeof article.comment_count).toBe("number");
           expect(typeof article.created_at).toBe("string");
           expect(typeof article.votes).toBe("number");
           expect(typeof article.topic).toBe("string");
@@ -174,7 +195,7 @@ describe("/api/articles", () => {
         expect(typeof article.article_id).toBe("number");
         expect(typeof article.title).toBe("string");
         expect(typeof article.author).toBe("string");
-        expect(typeof article.comment_count).toBe("string");
+        expect(typeof article.comment_count).toBe("number");
         expect(typeof article.created_at).toBe("string");
         expect(typeof article.votes).toBe("number");
         expect(typeof article.topic).toBe("string");
